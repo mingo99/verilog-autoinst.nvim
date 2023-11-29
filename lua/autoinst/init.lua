033c21daf729fa72f0c0dc01d251de89d1ee2fcb
mingo99 <2839131656@qq.com>
2023-11-29 14:16:02 +0800

    using treesitter to extract items

--- lua/autoinst/init.lua
+++ lua/autoinst/init.lua
@@ -1,63 +1,61 @@
 local Util = require("autoinst.util")
+local TS = vim.treesitter
 
-local function extract(path)
-	local f = assert(io.open(path, "r"))
-	local s = assert(f:read("*a"))
+local function get_query(stext, query_string)
+	local parser = TS.get_string_parser(stext, "verilog")
+	local ok, query = pcall(vim.treesitter.query.parse, parser:lang(), query_string)
 
-	-- Extract module head
-	local head_pattern = "(module.-%);)"
-	local head = s:match(head_pattern)
-	assert(head, "Error: Module not found")
-
-	-- Get module name
-	local name_pattern = "module%s+([%w_]+)%s*"
-	local module_name = head:match(name_pattern)
-
-	-- Get parameters
-	local params = {}
-	local param_pattern = "parameter%s+[%l%s%[%]%d:]*([%u%d_]+%s*=%s*%d+)"
-	local params_match = head:gmatch(param_pattern)
-	if params_match then
-		for param in params_match do
-			local name, value = param:match("([%w_]+)%s*=%s*(%w+)")
-			table.insert(params, { name, value })
-		end
+	if not ok then
+		print("Failed to parse query")
+		return
 	end
 
-	-- Get Port
-	local ports = {}
-	local port_pattern = "%b();"
-	local ports_match = head:match(port_pattern)
+	local tree = parser:parse()[1]
 
-	local port_modes = { "^o?[iu][nt][op]ut%s+%w+%s*%b[]%s*(.-)$", "^o?[iu][nt][op]ut%s+%w+%s*(.-)$" }
-	for line in ports_match:gmatch("%s*([^\r\n();]+)") do
-		for _, mode in ipairs(port_modes) do
-			local port = line:match(mode)
-			if port then
-				for p in port:gmatch("[^,%s%[%]%u]+") do
-					table.insert(ports, p)
-				end
-				break
-			end
-		end
+	local items = {}
+	for id, node, metadata in query:iter_captures(tree:root(), 0, 0, -1) do
+		local item = vim.treesitter.get_node_text(node, stext, metadata[id])
+		table.insert(items, item)
 	end
 
-	return { module_name, params, ports }
+	return items
 end
 
-local function geninst(module_name, params, ports)
+local function get_header_items(file_path)
+	local stext = io.open(file_path):read("*a")
+	local name = get_query(stext, "(module_header (simple_identifier) @module_name)")
+	local params = {
+		idents = get_query(stext, "(param_assignment (parameter_identifier) @param_name )"),
+		consts = get_query(stext, "(param_assignment (constant_param_expression) @param_value)"),
+	}
+
+	local ports = get_query(stext, "(ansi_port_declaration (port_identifier) @port_name)")
+
+	if not name or not params or not ports then
+		vim.notify("Failed to get query")
+		return
+	end
+
+	return {
+		name = name[1],
+		params = params,
+		ports = ports,
+	}
+end
+
+local function gen_inst(name, params, ports)
 	local inst_code = ""
-	if #params > 0 then
-		inst_code = inst_code .. module_name .. " #("
-		for i, param in ipairs(params) do
-			if i == #params then
-				inst_code = inst_code .. string.format(".%s(%s)) u_%s(", param[1], param[2], module_name)
+	if #params.idents > 0 then
+		inst_code = inst_code .. name .. " #("
+		for i, ident in ipairs(params.idents) do
+			if i == #params.idents then
+				inst_code = inst_code .. string.format(".%s(%s)) u_%s(", ident, params.consts[i], name)
 			else
-				inst_code = inst_code .. string.format(".%s(%s),", param[1], param[2])
+				inst_code = inst_code .. string.format(".%s(%s),", ident, params.consts[i])
 			end
 		end
 	else
-		inst_code = inst_code .. string.format("%s u_%s(", module_name, module_name)
+		inst_code = inst_code .. string.format("%s u_%s(", name, name)
 	end
 
 	for i, port in ipairs(ports) do
@@ -79,8 +77,12 @@ local function inst_with_path(path)
 		local root = Util.get_root()
 		full_path = root .. "/" .. path
 	end
-	local module = extract(full_path)
-	local inst_code = geninst(module[1], module[2], module[3])
+
+	local items = get_header_items(full_path)
+	if items == nil then
+		return
+	end
+	local inst_code = gen_inst(items.name, items.params, items.ports)
 	vim.api.nvim_put({ inst_code }, "", false, true)
 end
 
